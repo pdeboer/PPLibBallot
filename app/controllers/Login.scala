d5633bad96a6f1939d02607bea09f36069290bf7
Patrick de Boer <pdeboer@mit.edu>
2015-11-18 14:28:55 +0100

    indention

--- app/controllers/Login.scala
+++ app/controllers/Login.scala
@@ -6,28 +6,28 @@ import play.api.mvc._
 
 object Login extends Controller {
 
-  def logout = Action { request =>
-    Ok(views.html.login()).withNewSession
-  }
+	def logout = Action { request =>
+		Ok(views.html.login()).withNewSession
+	}
 
-  def login = Action { request =>
-    val turkerId = getTurkerIDFromRequest(request)
+	def login = Action { request =>
+		val turkerId = getTurkerIDFromRequest(request)
 
-    if(UserDAO.findByTurkerId(turkerId).isEmpty) {
-      UserDAO.create(turkerId, new DateTime())
-    }
+		if (UserDAO.findByTurkerId(turkerId).isEmpty) {
+			UserDAO.create(turkerId, new DateTime())
+		}
 
-    // Redirect if necessary otherwise just go to index
-    request.session.get("redirect").map { redirect =>
-      Redirect(redirect).withSession(request.session - "redirect" + ("TurkerID" -> turkerId))
-    }.getOrElse {
-      Ok(views.html.index(turkerId)).withSession(request.session + ("TurkerID" -> turkerId))
-    }
-  }
+		// Redirect if necessary otherwise just go to index
+		request.session.get("redirect").map { redirect =>
+			Redirect(redirect).withSession(request.session - "redirect" + ("TurkerID" -> turkerId))
+		}.getOrElse {
+			Ok(views.html.index(turkerId)).withSession(request.session + ("TurkerID" -> turkerId))
+		}
+	}
 
 
-  def getTurkerIDFromRequest(request: Request[AnyContent]): String = {
-    request.queryString.getOrElse("TurkerID",List("")).head
-    //request.body.asFormUrlEncoded.get("TurkerID").head
-  }
+	def getTurkerIDFromRequest(request: Request[AnyContent]): String = {
+		request.queryString.getOrElse("TurkerID", List("")).head
+		//request.body.asFormUrlEncoded.get("TurkerID").head
+	}
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -1,46 +1,44 @@
 package models
 
-import anorm._
 import anorm.SqlParser._
+import anorm._
 import org.joda.time.DateTime
-import play.api.db.DB
 import play.api.Play.current
-
-import scala.collection.mutable
+import play.api.db.DB
 
 /**
- * Created by mattia on 02.07.15.
- */
+  * Created by mattia on 02.07.15.
+  */
 case class User(id: Pk[Long], turkerId: String, firstSeenDateTime: DateTime)
 
 object UserDAO {
-  private val userParser: RowParser[User] =
-    get[Pk[Long]]("id") ~
-      get[String]("turker_id") ~
-      get[DateTime]("first_seen_date_time") map {
-      case id ~turker_id ~first_seen_date_time =>
-        User(id, turker_id, first_seen_date_time)
-    }
+	private val userParser: RowParser[User] =
+		get[Pk[Long]]("id") ~
+				get[String]("turker_id") ~
+				get[DateTime]("first_seen_date_time") map {
+			case id ~ turker_id ~ first_seen_date_time =>
+				User(id, turker_id, first_seen_date_time)
+		}
 
-  def findById(id: Long): Option[User] =
-    DB.withConnection { implicit c =>
-      SQL("SELECT * FROM user WHERE id = {id}").on(
-        'id -> id
-      ).as(userParser.singleOpt)
-    }
+	def findById(id: Long): Option[User] =
+		DB.withConnection { implicit c =>
+			SQL("SELECT * FROM user WHERE id = {id}").on(
+				'id -> id
+			).as(userParser.singleOpt)
+		}
 
-  def findByTurkerId(turkerId: String): Option[User] =
-    DB.withConnection { implicit c =>
-      SQL("SELECT * FROM user WHERE turker_id = {turkerId}").on(
-        'turkerId -> turkerId
-      ).as(userParser.singleOpt)
-    }
+	def findByTurkerId(turkerId: String): Option[User] =
+		DB.withConnection { implicit c =>
+			SQL("SELECT * FROM user WHERE turker_id = {turkerId}").on(
+				'turkerId -> turkerId
+			).as(userParser.singleOpt)
+		}
 
-  def create(turkerId: String, firstSeenDateTime: DateTime): Option[Long] =
-    DB.withConnection { implicit c =>
-      SQL("INSERT INTO user(turker_id, first_seen_date_time) VALUES ({turkerId}, {firstSeenDateTime})").on(
-        'turkerId -> turkerId,
-        'firstSeenDateTime -> firstSeenDateTime
-      ).executeInsert()
-    }
+	def create(turkerId: String, firstSeenDateTime: DateTime): Option[Long] =
+		DB.withConnection { implicit c =>
+			SQL("INSERT INTO user(turker_id, first_seen_date_time) VALUES ({turkerId}, {firstSeenDateTime})").on(
+				'turkerId -> turkerId,
+				'firstSeenDateTime -> firstSeenDateTime
+			).executeInsert()
+		}
 }
